5e56b20c207b8708ed58e98313dae2bf3ff7dff6
Ryan Hogan <ryanoman2011@hotmail.com>
2023-11-27 19:53:53 +0000

    Improved the home page.

--- src/components/content.js
+++ src/components/content.js
@@ -1,8 +1,19 @@
 function Content() {
   return (
-      <div>
-          <h1>Test</h1>
-      </div>
+    <div>
+      <br/>
+      <br/>
+      <br/>
+      <h2>Welcome</h2>
+      <p>
+        Here on this site you can add films you've watched to your list, 
+        edit previous additions or browse all of the films you've seen in your list.
+      </p>
+      <br/>
+      <p>
+        Please navigate to one of the options in the bar at the top of the screen to get started.
+      </p>
+    </div>
   );
 }
 export default Content;
