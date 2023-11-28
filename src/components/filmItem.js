2dfab592452675b218190b18df66b83909d7b370
Ryan Hogan <ryanoman2011@hotmail.com>
2023-11-28 17:59:50 +0000

    Changed the styling a bit.

--- src/components/filmItem.js
+++ src/components/filmItem.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
 
@@ -5,19 +6,20 @@ function FilmItem(props) {
   return (
     <div>
       <center>
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src={props.myFilm.cover} />   {/* Set the card image to the cover of the film.       */}
+        <Card style={{height: '100%'}}>
+          {/* Set the card image to the cover of the film. */}
+          <Card.Img variant="top" src={props.myFilm.cover} style={{height: '500px'}} />
           <Card.Body>
-            <Card.Title>{props.myFilm.title}</Card.Title>       {/* Set the card title to the title of the film.       */}
-            <Card.Text>
-              {props.myFilm.director}                           {/* Set the card director to the director of the film. */}
-            </Card.Text>
+            {/* Set the card title to the title of the film. */}
+            <Card.Title>{props.myFilm.title}</Card.Title>
+            {/* Set the card director to the director of the film. */}
+            <Card.Text>{props.myFilm.director}</Card.Text>
           </Card.Body>
           <Link to={'/update/' + props.myFilm._id} className='btn btn-primary'>Update</Link>
         </Card>
       </center>
     </div>
-  )
-}
+  );
+} 
 
 export default FilmItem;
