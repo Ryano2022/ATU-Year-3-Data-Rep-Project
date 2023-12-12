f6380b393087c6336c601599921aed5792a26ff5
Ryan Hogan <ryanoman2011@hotmail.com>
2023-12-12 18:57:06 +0000

    Now displays all three images.

--- src/components/filmItem.js
+++ src/components/filmItem.js
@@ -1,16 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import axios from 'axios';
 
 function FilmItem(props) {
+  const [posterNum, setPosterNum] = useState(1);
+
+  useEffect(
+    () => {
+      const interval = setInterval(nextPoster, 3000);
+      return () => clearInterval(interval);
+    }, 
+    [posterNum]
+  );
+
+  const nextPoster = () => {
+    if (posterNum + 1 >= 4) {
+      setPosterNum(1);
+    } 
+    else {
+      setPosterNum(posterNum + 1);
+    }
+  };
+
+  var posterSrc;
+  if (posterNum === 1) {
+    posterSrc = props.myFilm.poster1;
+  } 
+  else if (posterNum === 2) {
+    posterSrc = props.myFilm.poster2;
+  } 
+  else if (posterNum === 3) {
+    posterSrc = props.myFilm.poster3;
+  }
+
   return (
     <div>
       <center>
         <Card style={{ height: '100%' }}>
           {/* Set the card image to the poster of the film. */}
-          <Card.Img variant="top" src={props.myFilm.poster1} style={{ height: '500px' }} />
+          <Card.Img variant="top" src={posterSrc} style={{ height: '500px' }} />
           <Card.Body>
             {/* Set the card title to the title of the film. */}
             <Card.Title>{props.myFilm.title}</Card.Title>
--- src/components/films.js
+++ src/components/films.js
@@ -8,8 +8,8 @@ function Films(props) {
     <div className="container">
       <div className="row"> {/* Making it so that they can only be in rows. */}
         {myFilms.map((film) => (
-          <div className="col-md-4 mb-4">
-            <FilmItem myFilm={film} key={film._id} Reload={()=>{props.DataReload()}}/> {/* Displaying entries from the collection.  */}
+          <div className="col-md-4 mb-4" key={film._id}>
+            <FilmItem myFilm={film} Reload={()=>{props.DataReload()}}/> {/* Displaying entries from the collection.  */}
           </div>
         ))}
       </div>
