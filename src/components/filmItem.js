fbf90489f975a482c34ded748b3bff7b9abcd118
Ryan Hogan <ryanoman2011@hotmail.com>
2023-12-13 13:01:17 +0000

    Buttons now sit side by side.

--- src/components/filmItem.js
+++ src/components/filmItem.js
@@ -3,7 +3,7 @@ import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import axios from 'axios';
-const placeholderImagePath = require('../images/placeholder.jpg'); // Import the default placeholder image.
+const placeholderImagePath = require('../images/placeholder.jpg');   // Import the default placeholder image.
 const placeholderImagePath1 = require('../images/placeholder1.jpg'); // Import the first placeholder image.
 const placeholderImagePath2 = require('../images/placeholder2.jpg'); // Import the second placeholder image.
 const placeholderImagePath3 = require('../images/placeholder3.jpg'); // Import the third placeholder image.
@@ -15,14 +15,14 @@ function FilmItem(props) {
     () => {
       const interval = setInterval(nextPoster, 3000);
       return () => clearInterval(interval);
-    }, 
+    },
     [posterNum]
   );
 
   const nextPoster = () => {
     if (posterNum + 1 >= 4) {
       setPosterNum(1);
-    } 
+    }
     else {
       setPosterNum(posterNum + 1);
     }
@@ -32,10 +32,10 @@ function FilmItem(props) {
 
   if (posterNum === 1) {
     posterSrc = props.myFilm.poster1 || placeholderImagePath1; // Assign a placeholder if poster1 is not available.
-  } 
+  }
   else if (posterNum === 2) {
     posterSrc = props.myFilm.poster2 || placeholderImagePath2; // Assign a placeholder if poster2 is not available.
-  } 
+  }
   else if (posterNum === 3) {
     posterSrc = props.myFilm.poster3 || placeholderImagePath3; // Assign a placeholder if poster3 is not available.
   }
@@ -55,16 +55,20 @@ function FilmItem(props) {
             {/* Set the card director to the director of the film. */}
             <Card.Text>{props.myFilm.director}</Card.Text>
           </Card.Body>
-          <Link to={'/update/' + props.myFilm._id} className='btn btn-primary'>Update</Link>
-          <Button variant='danger' onClick={() => {
-            if (window.confirm("Are you sure you want to delete this film?")) {
-              axios.delete('http://localhost:4000/api/film/' + props.myFilm._id)
-                .then(() => {
-                  props.Reload();
-                })
-                .catch();
-            }
-          }}>Delete</Button>
+          <div className="button-container">
+            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+              <Link to={'/update/' + props.myFilm._id} className='btn btn-primary' style={{ width: '50%', borderRadius: '0' }}>Update</Link>
+              <Button variant='danger' onClick={() => {
+                if (window.confirm("Are you sure you want to delete this film?")) {
+                  axios.delete('http://localhost:4000/api/film/' + props.myFilm._id)
+                    .then(() => {
+                      props.Reload();
+                    })
+                    .catch();
+                }
+              }} style={{ width: '50%', borderRadius: '0' }}>Delete</Button>
+            </div>
+          </div>
         </Card>
       </center>
     </div>
